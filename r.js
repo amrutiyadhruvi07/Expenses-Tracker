f579f95da52138e085be5486b3e634ac2b9dd94d
dhruvi amrutiya <amrutiyadhruvi07@gmail.com>
2026-06-27 12:24:19 +0530

    Add files via upload

---
+++ r.js
@@ -0,0 +1,14 @@
+
+function showName(){
+
+let date=document.getElementById("date").value;
+let amount=document.getElementById("amount").value;
+let text=document.getElementById("text").value;
+
+
+document.getElementById("output").innerText= date+ " : "+ "I was spent " + amount + " on " + text;
+}
+
+
+
+
